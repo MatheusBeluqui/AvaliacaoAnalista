be2e9da9d4e33e5d30be52085b25a90d7c26a36b
leandro.ekamoto <leandro.ekamoto@castgroup.com.br>
2024-07-26 18:09:24 -0300

    Solução das atividades 1 e 2 com testes, respostas do questionário

--- Teste2/Program.cs
+++ Teste2/Program.cs
@@ -6,13 +6,98 @@
     Obs: A seguência de Fibonacci segue o padrão 1,1,2,3,5,8 e assim por diante, sendo o próximo número a soma do último número com o seu anterior.
 */
 
+using System.Diagnostics;
+using System.Text;
+
 namespace Teste2
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+
+        public static string GetFibonacciSequence(int max)
         {
-            Console.WriteLine("Boa sorte!");
+            StringBuilder sequence = new StringBuilder();
+            int count = 1;
+            AddFibonacciNumbers(max, 1, 1, sequence, count);
+
+            if (sequence.Length > 1)
+                sequence.Remove(sequence.Length - 1, 1);
+
+            return sequence.ToString();
+        }
+
+        private static void AddFibonacciNumbers(int max, int a, int b, StringBuilder sequence, int count)
+        {
+            if (count > max)
+                return;
+
+            sequence.Append(a).Append(",");
+            AddFibonacciNumbers(max, b, a + b, sequence, count+1);
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////
+        public static string? Fibonacci(int quantidadeNumeros) 
+        {
+            StringBuilder sequence = new StringBuilder();
+            int a = 1;
+            int b = 1;
+
+            if (a <= quantidadeNumeros)
+                sequence.Append(a);
+
+            int contador = 1;
+
+            while (contador < quantidadeNumeros)
+            {
+                sequence.Append(",").Append(b);
+                int temp = a;
+                a = b;
+                b = temp + b;
+                contador++;
+            }
+
+            return sequence.ToString();
+        }
+
+        public static string? Run(string? entrada)
+        {
+            int quantidadeNumeros;
+            string? resultado = string.Empty;
+
+            if (int.TryParse(entrada, out quantidadeNumeros))
+            {
+                if (quantidadeNumeros < 0)
+                    return "Valor passado não pode ser menor que zero";
+
+                if (quantidadeNumeros == 0)
+                    return "Valor passado não pode ser zero";
+
+                if (quantidadeNumeros > 19)
+                    return "Valor passado não pode ser maior que 20";
+
+                // Recurviso desempenho exponencial (O(2^n))
+                // resultado = GetFibonacciSequence(quantidadeNumeros);
+
+                // Linear desempenho (O(n))
+                resultado = Fibonacci(quantidadeNumeros);
+
+                return resultado;
+            }
+            else
+                return "Valor passado não é um número inteiro";
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com algum valor:");
+            string? entrada = Console.ReadLine();
+
+            var result = Run(entrada);
+            Console.WriteLine("Resultado:");
+            Console.WriteLine(result);
+
+            Console.WriteLine("Digite qualquer coisa para fechar");
+            Console.ReadLine();
         }
     }
 }
