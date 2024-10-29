8d0595486e61ee41e929cc9f4e7895c3607e5044
spanol <viniciusspanol@gmail.com>
2024-10-29 19:47:09 -0300

    🔧fix: Removido import desnecessário

--- Teste2/Program.cs
+++ Teste2/Program.cs
@@ -6,7 +6,6 @@
     Obs: A seguência de Fibonacci segue o padrão 1,1,2,3,5,8 e assim por diante, sendo o próximo número a soma do último número com o seu anterior.
 */
 
-using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace Teste2
