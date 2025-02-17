4d3981afb8d41a242466021f225fe8aa1fc93dac
Gabriel Vitor <gabriel.xavier@alfasoft.pt>
2025-02-17 12:28:40 -0300

    ana basoli respostas

--- Teste1/Program.cs
+++ Teste1/Program.cs
@@ -5,13 +5,38 @@
     Exemplo: AAABCCDDD -> Retorno : ABCD
  */
 
+using System.Text;
+
 namespace Teste1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Boa sorte!");
+            string str;
+
+            do {
+                Console.WriteLine("Digite uma string alfanumérica: ");
+                str = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(str) && str.All(c => char.IsLetterOrDigit(c))) {
+                    str = str.ToUpper();
+
+                    StringBuilder sb = new StringBuilder(str[0].ToString());
+
+                    foreach (char c in str)
+                    {
+                        if (sb[sb.Length - 1] != c) sb.Append(c);
+                    }
+
+                    Console.WriteLine($"Seu resultado: {sb.ToString()}");
+                    break;
+                } 
+                else {
+                    Console.WriteLine("Sua string deve conter apenas letras e numeros!");
+                }
+
+            } while (true);
         }
     }
 }
