edf63189f953b46f32bb372be8e1e82af5f7e6f2
robertomedeiros1976 <robertomedeiros@outlook.com>
2025-02-19 01:12:29 -0300

    Teste concluido

--- Teste1/Program.cs
+++ Teste1/Program.cs
@@ -5,13 +5,43 @@
     Exemplo: AAABCCDDD -> Retorno : ABCD
  */
 
+using System.Text;
+
 namespace Teste1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Boa sorte!");
+            Console.WriteLine("Digite um texto qualquer, com letras e/ou números");
+            
+			string input = Console.ReadLine();
+            string result = RemoveNearDulicate(input);
+
+            Console.WriteLine($"Retorno sem repetidos {result}");
         }
+
+        /// <summary>
+        /// Function that will remove identical near charecters
+        /// </summary>
+        /// <param name="input">Value that is passed by user</param>
+        /// <returns>New string with no chars repeated</returns>
+        static string RemoveNearDulicate(string input)
+        {
+            if (string.IsNullOrEmpty(input)) return input;
+            char[] chars = input.ToCharArray(); 
+            char prevChar = chars[0];
+            string output = prevChar.ToString();
+
+			for (int i = 0; i < chars.Length; i++)
+			{
+                if (!chars[i].Equals(prevChar)) { 
+                    output += chars[i];
+                    prevChar = chars[i];
+                }
+			}
+
+            return output;
+		}
     }
 }
--- Teste2/Program.cs
+++ Teste2/Program.cs
@@ -12,7 +12,39 @@ namespace Teste2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Boa sorte!");
+            Console.WriteLine("Digite um número inteiro e maior que zero...");
+            int input;
+
+            while (!int.TryParse(Console.ReadLine(), out input) || input <= 0 || input > 20) 
+            {
+				Console.WriteLine("Entrada inválida. Por favor, digite um número inteiro positivo, maior que 0 e menor que 20:");
+			}
+
+            var output = GetSequence(input);
+
+            Console.WriteLine($"A sequência gerada é {output}");
+        }
+
+        static int SumValues(List<int> values)
+        {
+            int penultimateValue = values[values.Count - 2];
+            int lastValue = values[values.Count - 1];
+
+            return penultimateValue + lastValue;
+        }
+
+        static string GetSequence(int input)
+        {
+            List<int> output = new List<int>();
+            for (int i = 0; i < input; i++)
+            {
+                int currentValue = output.Count < 2 ? 1 : SumValues(output);
+                output.Add(currentValue);
+            }
+
+            return String.Join(" ", output);
         }
     }
+
+
 }
